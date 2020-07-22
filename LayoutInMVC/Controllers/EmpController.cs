8d8678e424f78c8913be8927dd41c552ef149f2b
Aniketrana <ranaannu93@gmail.com>
2020-07-22 11:45:37 +0530

    Add project files.

---
+++ LayoutInMVC/Controllers/EmpController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutInMVC.Controllers
+{
+    public class EmpController : Controller
+    {
+        [Route("AllEmp")]
+        public ActionResult GetList() 
+        {
+            return View();
+        }
+
+        [Route("NewEmp")]
+        public ActionResult AddEmp()
+        {
+            return View();
+        }
+    }
+}
